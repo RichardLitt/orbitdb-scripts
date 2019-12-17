7f542f8ed5832e8aa9c78845e3779c1842ae7ca9
Richard Littauer <richard+github@burntfen.com>
2019-12-17 06:33:17 -0500

    Automated before and after dates

--- run-quarterly-numbers.sh
+++ run-quarterly-numbers.sh
@@ -2,23 +2,23 @@
 # It needes to be edited, but keeping this here for next time should save some life.
 # This could easily be extended elsewhere.
 
-npm i -g name-your-contributors
+# npm i -g name-your-contributors
 # You will also need to have [`jq`](https://stedolan.github.io/jq/download/) installed.
-brew install jq
+# brew install jq
 
 # Fix these to match the current month
 # TODO Automate monthly so you don't need to do 'before' and 'after'
-name-your-contributors -o orbitdb -a 2019-05-31 -b 2019-07-01 --commits > orbitdb-june-contribs.json
-name-your-contributors -o orbitdb -a 2019-06-31 -b 2019-08-01 --commits > orbitdb-june-contribs.json
-name-your-contributors -o orbitdb -a 2019-07-31 -b 2019-09-01 --commits > orbitdb-june-contribs.json
-
+# GITHUB_TOKEN=1ffa490b775ba233e8b46b111d57a9ebd82fbbfe name-your-contributors -o orbitdb -m June --commits > orbitdb-june-contribs.json
+# Fix these to match the current month
+name-your-contributors -o orbitdb -m October --commits > orbitdb-oct-contribs.json
+name-your-contributors -o orbitdb -m November --commits > orbitdb-nov-contribs.json
+name-your-contributors -o orbitdb -m December --commits > orbitdb-dec-contribs.json
 # Change these to match the output above
-cat orbitdb-june-contribs.json | jq '[ .[][].count]' | jq 'add | "There were \(.) contributions (including commits, comments and PRs) in June."'
-cat orbitdb-july-contribs.json | jq '[ .[][].count]' | jq 'add | "There were \(.) contributions (including commits, comments and PRs) in July."'
-cat orbitdb-aug-contribs.json | jq '[ .[][].count]' | jq 'add | "There were \(.) contributions (including commits, comments and PRs) in August."'
-
+cat orbitdb-oct-contribs.json | jq '[ .[][].count]' | jq 'add | "There were \(.) contributions (including commits, comments and PRs) in October."'
+cat orbitdb-nov-contribs.json | jq '[ .[][].count]' | jq 'add | "There were \(.) contributions (including commits, comments and PRs) in November."'
+cat orbitdb-dec-contribs.json | jq '[ .[][].count]' | jq 'add | "There were \(.) contributions (including commits, comments and PRs) in December."'
 # These will just print out the unique numbers.
 # Change these to match the output above.
-cat orbitdb-june-contribs.json | jq '[ .[][].login]' | jq unique | jq -r .[] | wc -l | awk '{print "There were "$1" contributors in June."}'
-cat orbitdb-july-contribs.json | jq '[ .[][].login]' | jq unique | jq -r .[] | wc -l | awk '{print "There were "$1" contributors in July."}'
-cat orbitdb-aug-contribs.json | jq '[ .[][].login]' | jq unique | jq -r .[] | wc -l | awk '{print "There were "$1" contributors in August."}'
+cat orbitdb-oct-contribs.json | jq '[ .[][].login]' | jq unique | jq -r .[] | wc -l | awk '{print "There were "$1" contributors in October."}'
+cat orbitdb-nov-contribs.json | jq '[ .[][].login]' | jq unique | jq -r .[] | wc -l | awk '{print "There were "$1" contributors in November."}'
+cat orbitdb-dec-contribs.json | jq '[ .[][].login]' | jq unique | jq -r .[] | wc -l | awk '{print "There were "$1" contributors in December."}'
